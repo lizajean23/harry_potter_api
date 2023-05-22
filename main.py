41411fa7ff091572be0369a8ef7ed32a5c253f3c
liziko <lizi.guchua.1@btu.edu.ge>
2023-05-22 23:27:47 +0400

    windows notification

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import requests
 import sqlite3
 import json
+import win10toast
+from datetime import datetime
 
-# 1
+#
+# # 1
 character = requests.get('https://hp-api.onrender.com/api/characters')
 print(character.headers)
 print(character.text)
 print(character.content)
-
-# 2
+#
+# # 2
 characters = character.json()
 with open('harry_potter.json', 'w') as file:
     json.dump(characters, file, indent=4)
-
-# 3
+#
+# # 3
 values = []
 index = 0
 for each in characters:
@@ -30,8 +33,8 @@ for each in characters:
     values.append(value)
     index += 1
     print(value)
-
-# 4 (მონაცემები ჰარი პოტერის პერსონაჟების შესახებ)
+#
+# # 4 (მონაცემები ჰარი პოტერის პერსონაჟების შესახებ)
 con = sqlite3.connect('harry_potter.sqlite3')
 cursor = con.cursor()
 
@@ -57,3 +60,35 @@ cursor.executemany(
     values)
 
 con.commit()
+
+
+# toast notification about holidays
+def holiday():
+    toast = win10toast.ToastNotifier()
+    api_key = '9d88ff3357945c5bf1162ae5d5db6abd213e2d08'
+    url = 'https://calendarific.com/api/v2/holidays'
+    response = requests.get(f'https://calendarific.com/api/v2/holidays?&api_key={api_key}&country=GE&year=2023')
+    # print(response.json())
+    result = response.json()
+    res = json.dumps(result, indent=4)
+    current_date = str(datetime.now().date())
+    current_date = str(datetime(2023, 5, 26).date())
+
+    is_holiday = False
+    for num in range(len(result['response']['holidays'])):
+        date = result['response']['holidays'][num]['date']['iso']
+        holiday_name = result['response']['holidays'][num]['name']
+        if date == current_date:
+            is_holiday = True
+            toast.show_toast(title=f"YAAAAAY TODAY YOU CAN REST {current_date} and it is a {holiday_name}", msg=f"{holiday_name}",
+                             duration=5)
+            print(date, holiday_name)
+            break
+    if not is_holiday:
+        toast.show_toast(title=f"today is {current_date} and it is not a holiday", msg=f"{current_date}", duration=5)
+
+    # print(res)
+    # print(type(current_date))
+
+
+holiday()
